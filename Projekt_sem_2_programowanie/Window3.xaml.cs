a18efef857b2d58f504690736655c5d7d9048d18
Marcin Góral <marcin.goral@microsoft.wsei.edu.pl>
2023-07-17 20:15:37 +0200

    Wprowadzanie danych

--- Projekt_sem_2_programowanie/Window3.xaml.cs
+++ Projekt_sem_2_programowanie/Window3.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Projekt_sem_2_programowanie
 {
@@ -23,8 +24,37 @@ namespace Projekt_sem_2_programowanie
         public Window3()
         {
             InitializeComponent();
+            LoadGrid();
         }
 
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-Q063QP0;Initial Catalog=DB_2023_07;Integrated Security=True;Encrypt=False");
+        private void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Awarie", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+        
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Awarie (Wtryskarka_Id, Person_Name, KosztAwarii) VALUES (@Wtryskarka_Id, @Person_Name, @KosztAwarii)", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@Wtryskarka_Id", WtryskarkaText.Text);
+            cmd.Parameters.AddWithValue("@Person_Name", PersonName.Text);
+            cmd.Parameters.AddWithValue("@KosztAwarii", KosztAwarii.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            LoadGrid();
+            MessageBox.Show("New Record Inserted Successfully");
+        }
         private void btnOpenWindow1_Click(object sender, RoutedEventArgs e)
         {
             MainWindow window1 = new MainWindow();
@@ -37,7 +67,6 @@ namespace Projekt_sem_2_programowanie
                 }
             }
         }
-
         private void btnOpenWindow2_Click(object sender, RoutedEventArgs e)
         {
             Window2 window2 = new Window2();
